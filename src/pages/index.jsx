56ee6422c3b41d84d563eb6f90c99d14f8e46827
Murillo Cesar <murillo.devops@gmail.com>
2023-10-28 14:34:27 -0300

    transformando modelo para user typescript

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
-import Presente from "../components/Presente";
 import Porta from "../components/Porta";
+import PortaModel from "../model/portaModel";
+import {useState} from "react";
 
 export default function Home() {
+    const [p1,setP1] = useState(new PortaModel(1))
+
     return (
         <div style={{display: "flex"}}>
-            <Porta selecionada/>
-            <Porta/>
+            <Porta porta={p1} />
         </div>
     )
 }
