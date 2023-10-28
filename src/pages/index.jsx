eec43e1ba6e3168aeee9e0451ec230734f2b2e88
Murillo Cesar <murillo.devops@gmail.com>
2023-10-28 11:51:41 -0300

    porta

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import Presente from "../components/Presente";
+import Porta from "../components/Porta";
+
+export default function Home() {
+    return (
+        <div style={{display: "flex"}}>
+            <Porta/>
+            <Porta/>
+        </div>
+    )
+}
